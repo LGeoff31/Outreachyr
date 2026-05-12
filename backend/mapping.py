e5c9f2c04f428449ce60a50b02580e152c610990
LGeoff31 <geoffrey31415@gmail.com>
2026-05-12 11:46:54 -0700

    add to mapping.py

--- backend/mapping.py
+++ backend/mapping.py
@@ -2,5 +2,6 @@ COMPANY_EMAIL_HOST = {
     "nvidia": "career.nvidia.com",
     "point72": "point72.com",
     "palantir": "palantir.com",  # first name LETTER + last name @ palantir.com
-    "cockroach": "cockroachlabs.com"
+    "cockroach": "cockroachlabs.com",
+    "gemini": "gemini.com"
 }
