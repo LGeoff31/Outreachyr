31b0e258d5762ffb9126eace68c30d06058b6937
Dominik Ritz <dom_ritz@icloud.com>
2026-05-12 17:38:12 -0400

    update COMPANY_EMAIL_HOST in mapping.py to include additional domains for Roblox, Amazon, and Shopify

--- backend/mapping.py
+++ backend/mapping.py
@@ -3,5 +3,8 @@ COMPANY_EMAIL_HOST = {
     "point72": "point72.com",
     "palantir": "palantir.com",  # first name LETTER + last name @ palantir.com
     "cockroach": "cockroachlabs.com",
-    "gemini": "gemini.com"
+    "gemini": "gemini.com",
+    "roblox": "roblox.com",
+    "amazon": "amazon.jobs",
+    "shopify": "shopify.com",
 }
